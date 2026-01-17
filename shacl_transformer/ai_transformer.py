12a433f61e879230d50a2dad9e2ca6ccd1d98ef5
Kurt Cagle <kurt.cagle@gmail.com>
2026-01-16 19:00:58 -0800

    Revise SHACL transformation guidelines
    Updated SHACL transformation guidelines to include new requirements for shape properties and namespaces.

--- shacl_transformer/ai_transformer.py
+++ shacl_transformer/ai_transformer.py
@@ -269,9 +269,19 @@ Generate schemas compliant with https://www.w3.org/TR/shacl12-core/ including:
 - SHACL Rules for calculated properties
 - UI Annotations (sh:group, sh:order, sh:defaultValue)
 - ALL shape nodes MUST use IRIs (no blank nodes)
-- sh:name, sh:codeIdentifier, and sh:description for all shapes
+- Use sh:name, sh:codeIdentifier, and sh:description for all shapes
 - Use SKOS for enumerations
 - Link to external vocabularies (Wikidata, DBpedia, etc.) where appropriate
+- Include UI annotations (sh:group, sh:order, shui:* predicates)
+- All classes and property shape identifiers should use descriptive IRIs rather than blank nodes
+- Schema conversion sources that include imports should incorporate those imports before conversion to SHACL
+- Include sh:message and sh:severity fields within properties, with severity determined contextually
+- Be as descriptive as possible for sh:description fields
+- Include sh:nodeKind statements in shape properties
+- Create namespaces based upon classes (i.e., PREFIX Person: <http://www.example.com/ns/Person#> )
+- Parameters can include a base namespace (baseNamespace) that is used to construct namespaces, which should be terminated with a "/" character. The default base namespace is "http://example.com/ns/".
+- Class and property shapes should incorporate xsh:altLabel, xsh:acronym, and xsh:soundsLike, where xsh: = <http:/example.com/ns/xsh#>. All of these should be lower case.
+- The xsh:soundsLike predicate uses the double-homophone encoding to create sounds-like representation of the primary and secondary labels.
 
 Output format: Valid RDF Turtle that can be parsed by rdflib."""
     
